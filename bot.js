312a77ec717c303a52a9989f0e4880874cee7075
Юрий Лисов <yulisov@fil-it.ru>
2021-10-19 16:20:35 +0300

    start project, add env config

---
+++ bot.js
@@ -0,0 +1,12 @@
+import { Telegraf } from 'telegraf'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+
+bot.launch()
