b6eb1e1dbabd3bc780d2b24182f9b92b124eb795
Юрий Лисов <yulisov@fil-it.ru>
2021-10-28 17:25:45 +0300

    fix

--- bot-actions.js
+++ bot-actions.js
@@ -28,8 +28,7 @@ async function showSpecificGif(ctx) {
 }
 
 function sendOptionsKeyboard(ctx, bot, questionMessage) {
-  console.log(bot)
-  bot.telegram.sendMessage(ctx.chat.id, questionMessage, {
+  bot.Composer.sendMessage(ctx.chat.id, questionMessage, {
     reply_markup: {
       inline_keyboard: [
         [
